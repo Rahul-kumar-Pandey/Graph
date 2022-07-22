7cd000cd39a64bc5bd013c236f8a1f2cf3a68f17
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2022-07-22 20:30:09 +0530

    Update and rename 11. Bipartiteness_of_undirected_graph.cpp to 011. Bipartiteness_of_undirected_graph.cpp

---
+++ 011. Bipartiteness_of_undirected_graph.cpp
@@ -0,0 +1,82 @@
+//BIPARTITE GRAPH: I CAN DIVIDE ALL VERTICES IN TWO SETS(SET1 AND SET2) SUCH THAT ALL EDGES ARE FROM SET1 TO SET2
+//IDEA: TWO COLORING METHOD (EVERY VERTEX OF SET1 HAS SAME COLOR AND EVERY VERTEX OF SET2 HAS SAME COLOR)
+
+
+/**************************using DFS********************************/
+bool bipartiteDfs(int node, vector<int> adj[], int color[]) {
+    for(auto it : adj[node]) {
+        if(color[it] == -1) {
+            color[it] = 1 - color[node];
+            if(!bipartiteDfs(it, adj, color)) {
+                return false; 
+            }
+        } else if(color[it] == color[node]) return false; 
+    }
+    return true; 
+}
+bool checkBipartite(vector<int> adj[], int n) {
+    int color[n];
+    memset(color, -1, sizeof color); 
+    for(int i = 0;i<n;i++) {
+        if(color[i] == -1) {
+            color[i] = 1;
+            if(!bipartiteDfs(i, adj, color)) {
+                return false;
+            }
+        } 
+    }
+    return true; 
+}
+
+
+/**************************using BFS********************************/
+bool bipartiteBfs(int src, vector<int> adj[], int color[]) {
+    queue<int>q;
+    q.push(src); 
+    color[src] = 1; 
+    while(!q.empty()) {
+        int node = q.front(); 
+        q.pop();
+        
+        for(auto it : adj[node]) {
+            if(color[it] == -1) {
+                color[it] = 1 - color[node]; 
+                q.push(it); 
+            } else if(color[it] == color[node]) {
+                return false; 
+            }
+        }
+    }
+    return true; 
+}
+bool checkBipartite(vector<int> adj[], int n) {
+    int color[n];
+    memset(color, -1, sizeof color); 
+    //taking all components of graph if in case graph is disconnected
+    for(int i = 0;i<n;i++) {
+        if(color[i] == -1) {
+            if(!bipartiteBfs(i, adj, color)) {
+                return false;
+            }
+        }
+    }
+    return true; 
+}
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> adj[n];
+	for(int i = 0;i<m;i++) {
+	    int u, v;
+	    cin >> u >> v;
+	    adj[u].push_back(v); 
+	    adj[v].push_back(u); 
+	}
+	
+	if(checkBipartite(adj, n)) {
+	    cout << "yes"; 
+	} else {
+	    cout << "No"; 
+	}
+	return 0;
+}
